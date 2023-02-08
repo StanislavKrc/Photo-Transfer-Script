1f7a27db9b825295cec0d4d350e420196702ad67
StanislavKrc <109104219+StanislavKrc@users.noreply.github.com>
2023-02-08 16:23:44 +0100

    Added input from file funktionality

--- phonedwnl/phonedwnl.sh
+++ phonedwnl/phonedwnl.sh
@@ -3,39 +3,64 @@ PDIR="$1"
 TDIR="$2"
 #prints help
 help () {
-    echo -e "\nFormat is: phonedwnl.sh source/help target flags"
+    echo -e "\nFormat is: \n
+        phonedwnl.sh source target 'transfer type arguments array'
+        phonedwnl.sh preset presetlist 'transfer type arguments array'
+    phonedwnl.sh -h"
     echo -e "\nFlags: \n"
     echo "-h : help, writes help once then ends script"
     echo "-p : copies photo folder"
     echo "-f : copies facebook folder"
     echo "-w : copies whatsapp folder"
     echo "-a : copies all listed above"
-    exit 0 
+    exit 0
 }
 : ' checks if first argument is given,and if both given directories exists
 in case they do,move to transfer type arguments'
 check () {
-
+    #argument is given
     if [ "$PDIR" != "" ]
     then
+        #directories doesnt exists
         if [ ! -d "$PDIR" ] || [ ! -d "$TDIR" ];
         then
+            #first argument is -h
             if [ "$1" = "-h" ]
             then
                 help
+                #first argument is something else
             else
-                echo -e "\n\t\tError: Directory wasnt found"
+                : 'echo -e "\n\t\tError: Directory wasnt found"
                 echo -e "\n\t\tGiven directories:\n\t\t$PDIR\n\t\t$TDIR"
-                exit 1
+                exit 1'
+                GetPath "$@"
             fi
+            #directories exists
         else
-            shift
-            shift
+            shift 2
             argParser "$@"
         fi
     fi
     
 }
+
+#check got non existent directory,looks for name in Directories.log file
+GetPath () {
+    in=$(cat "$TDIR")
+    filter="$PDIR"
+    PDIR=$(echo "$in" | awk -F ',' -v preset=$filter '{if($1 == preset){print $2}}')
+    TDIR=$(echo "$in" | awk -F ',' -v preset=$filter '{if($1 == preset){print $3}}')
+    if [ ! -d "$PDIR" ] || [ ! -d "$TDIR" ];
+    then
+        echo -e "\n\t\tError: Directory wasnt found"
+        echo -e "\n\t\tGiven directories:\n\t\t$PDIR\n\t\t$TDIR"
+        exit 1
+    else
+        shift 2
+        argParser "$@"
+    fi
+}
+
 #adds new subdirectory for TDIR based on current date
 newDir () {
     NDIR="$TDIR"/`date +%d.%m.%Y`
@@ -52,19 +77,19 @@ copyAll () {
     cp -R "${PDIR}/whatsapp/"* "$NDIR"
 }
 
- #copies photo subdirecotry
+#copies photo subdirecotry
 copyPhoto () {
     newDir
     cp -R "${PDIR}/photo/"* "$NDIR"
 }
 
- #copies whatsapp subdirecotry
+#copies whatsapp subdirecotry
 copyWA () {
     newDir
     cp -R "${PDIR}/whatsapp/"* "$NDIR"
 }
 
- #copies facebook subdirecotry
+#copies facebook subdirecotry
 copyFB () {
     newDir
     cp -R "${PDIR}/facebook/"* "$NDIR"
@@ -73,34 +98,34 @@ copyFB () {
 #switch function used to process transfer type arguments
 argParser () {
     case $1 in
-
+        
         -a)
             copyAll
         ;;
-
+        
         -p)
             copyPhoto
         ;;
-
+        
         -f)
             copyFB
         ;;
-
+        
         -w)
             copyWA
         ;;
-
+        
         *)
             echo -e "\n\t\t Error:Unknown command\n"
         ;;
-
+        
     esac
     shift
     if [ "$1" != "" ]
     then
         argParser "$@"
     else
-    echo -e "\n\tAll done,Script executed succesfully.\n"
+        echo -e "\n\tAll done,Script executed succesfully.\n"
         exit 0
     fi
 }
